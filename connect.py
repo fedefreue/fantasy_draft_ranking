8ef6c03f924c54c07a212d0dcd32c90b475ed569
fede <63209974+fedefreue@users.noreply.github.com>
2020-08-17 20:13:57 -0400

    Cleaned up connection script and added iteration over multiple seasons

--- connect.py
+++ connect.py
@@ -1,33 +1,53 @@
-from yahoo_oauth import OAuth2
+import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn.linear_model
 import yahoo_fantasy_api as yfa
+from yahoo_oauth import OAuth2
+
 import playerData
 
 #https://vicmora.github.io/blog/2017/03/17/yahoo-fantasy-sports-api-authentication
 
 conn = OAuth2(None, None, from_file='private.json')
+#conn.refresh_access_token()
 if not conn.token_is_valid():
     conn.refresh_access_token()
+
+#conn.session.get('https://fantasysports.yahooapis.com/fantasy/v2/league/XXX.l.25494')
 
 league = yfa.League(conn,'nfl.l.254924')
 
 #freeAgents = pd.DataFrame(league.free_agents(position))
 #freeAgentList = list(freeAgents['player_id'])
-seasonList = [2016, 2017, 2018, 2019]
-positions = ['RB', 'QB', 'WR', 'TE', 'K']
+seasonList = [2019]
+positions = ['RB', 'QB', 'WR', 'TE']
 points = {
-    'Pass Yds': 0.04,
-    'Pass TD': 4,
-    'Int': -1,
-    'Rush Att': 0.1,
-    'Rush TD': 6,
-    'Ret TD': 6,
-    'Rec': 1,
-    'Rec Yds': 0.1,
-    'Rec TD': 6,
-    '2-PT': 2,
-    'Fum Lost': -2,
-    'Fum Ret TD': 2
+    'Pass Yds':     0.04,
+    'Pass TD':      4,
+    'Int':          -1,
+    'Rush Att':     0.1,
+    'Rush TD':      6,
+    'Ret TD':       6,
+    'Rec':          1,
+    'Rec Yds':      0.1,
+    'Rec TD':       6,
+    '2-PT':         2,
+    'Fum Lost':     -2,
+    'Fum Ret TD':   2
 }
 
-playerData.generateTrainingTable(seasonList, league, points, positions)
+masterTable = playerData.generateTrainingTable(seasonList, league, points, positions)
+
+masterTable.to_csv('test_file.csv')
+
+features = masterTable.drop(columns = ['position_type', 'player_id', 'Points'])
+y_train = masterTable['Points']
+
+features = features.to_numpy()
+y_train = y_train.to_numpy()
+
+reg = sklearn.linear_model.LinearRegression().fit(features, y_train)
+
+print(reg.score(features, y_train))
+plt.scatter(reg.predict(features), y_train )
+plt.show()
--- playerData.py
+++ playerData.py
@@ -7,7 +7,7 @@ def pullStatsAndPoints(season: int, league: yfa.League, points: dict, position: 
     thisSeasonStats = pd.DataFrame(league.player_stats(players, 'season', season = season))
 
     # Calculate offensive points
-    thisSeasonStats['Points'] = thisSeasonStats['Pass Yds'] * points['Pass Yds'] + thisSeasonStats['Pass TD'] * points['Pass TD'] + thisSeasonStats['Int'] * points['Int'] + thisSeasonStats['Rush Att'] * points['Rush Att'] + thisSeasonStats['Rush TD'] * points['Rush TD'] + thisSeasonStats['Rec'] * points['Rec'] + thisSeasonStats['Rec Yds'] * points['Rec Yds'] + thisSeasonStats['Rec TD'] * points['Rec TD'] + thisSeasonStats['Ret TD'] * points['Ret TD'] + thisSeasonStats['2-PT'] * points['2-PT'] + thisSeasonStats['Fum Lost'] * points['Fum Lost'] + thisSeasonStats['Fum Ret TD'] * points['Fum Ret TD'] 
+    calculateOffensivePoints(thisSeasonStats, points) 
 
     thisSeasonPoints = thisSeasonStats[['player_id','Points']].copy()
     
@@ -16,6 +16,9 @@ def pullStatsAndPoints(season: int, league: yfa.League, points: dict, position: 
     df = df.drop(columns = ['name'])    
 
     return df
+
+def calculateOffensivePoints(thisSeasonStats, points):
+    thisSeasonStats['Points'] = thisSeasonStats['Pass Yds'] * points['Pass Yds'] + thisSeasonStats['Pass TD'] * points['Pass TD'] + thisSeasonStats['Int'] * points['Int'] + thisSeasonStats['Rush Att'] * points['Rush Att'] + thisSeasonStats['Rush TD'] * points['Rush TD'] + thisSeasonStats['Rec'] * points['Rec'] + thisSeasonStats['Rec Yds'] * points['Rec Yds'] + thisSeasonStats['Rec TD'] * points['Rec TD'] + thisSeasonStats['Ret TD'] * points['Ret TD'] + thisSeasonStats['2-PT'] * points['2-PT'] + thisSeasonStats['Fum Lost'] * points['Fum Lost'] + thisSeasonStats['Fum Ret TD'] * points['Fum Ret TD'] 
 
 def generateTrainingTable(seasonList: list, league: yfa.League, points: dict, positions: list):
     masterTable = pd.DataFrame()
