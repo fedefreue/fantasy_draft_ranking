4b0f6df0d25fe2d1975b773403ecb257050812b9
fedefreue <63209974+fedefreue@users.noreply.github.com>
2023-08-18 13:44:32 +0000

    Set up YFA connection and data dump display

--- app.py
+++ app.py
@@ -1,34 +1,88 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
+import yahoo_fantasy_api as yfa
+import pandas as pd
+import numpy as np
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+import connect
+import playerData
+
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
+points = {
+    "Pass Yds": 0.04,
+    "Pass TD": 4,
+    "Int": -1,
+    "Rush Att": 0.1,
+    "Rush TD": 6,
+    "Ret TD": 6,
+    "Rec": 1,
+    "Rec Yds": 0.1,
+    "Rec TD": 6,
+    "2-PT": 2,
+    "Fum Lost": -2,
+    "Fum Ret TD": 2,
+}
+seasonList = [2021, 2022, 2023]
+positionList = ["RB", "QB", "WR", "TE"]
+
+yfa_connection = None
+
+
+def model_train(masterTable: pd.DataFrame):
+    features = masterTable.drop(columns=["position_type", "player_id", "Points"])
+    y_train = masterTable["Points"]
+
+    features = features.to_numpy()
+    y_train = y_train.to_numpy()
+
+    model = sklearn.linear_model.LinearRegression().fit(features, y_train)
+    statusText = "R2: " + str(model.score(features, y_train))
+    # plt.scatter(self.model.predict(features), y_train)
+    # plt.show()
+
+
+@app.route("/", methods=["GET", "POST"])
 def route_home():
-	return render_template("home.html")
+    if request.form.get("connect") == "connect":
+        session["yfa_connection"] = connect.connect("private.json")
+        return render_template(
+            "home.html", buttonvalue=session.get("yfa_connection", None)
+        )
+    else:
+        return render_template("home.html", buttonvalue="Error")
 
-@app.route('/data/')
+
+@app.route("/data/", methods=["GET", "POST"])
 def route_data():
-	return render_template("data.html")
+    if request.form.get("gen_table") == "ok":
+        league_code = "nfl.l.209930"  # Get from a textbox
+        session["league_id"] = yfa.League(session.get("yfa_connection"), league_code)
+        data_table = playerData.generateTrainingTable(
+            seasonList, session.get("league_id", None), points, positionList
+        )
+        return render_template(
+            "data.html", tables=[data_table.to_html(classes="data", header="true")]
+        )
+    else:
+        return render_template("data.html")
 
-@app.route('/model/')
+
+@app.route("/model/", methods=["GET", "POST"])
 def route_model():
-	return render_template("model.html")
+    if request.form.get("train") == "train":
+        return render_template("model.html")
+    else:
+        return render_template("model.html")
 
-@app.route('/predict/')
+
+@app.route("/predict/")
 def route_predict():
-	return render_template("predict.html")
+    return render_template("predict.html")
 
 
-# main driver function
-if __name__ == '__main__':
-
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run()
+if __name__ == "__main__":
+    app.run()
--- connect.py
+++ connect.py
@@ -1,26 +1,31 @@
 from yahoo_oauth import OAuth2
+import json
 
-def connect(fileName:str):
+
+def connect(fileName: str):
     conn = OAuth2(None, None, from_file=fileName)
 
     if not conn.token_is_valid():
         conn.refresh_access_token()
-    
+
     return conn
 
+
 seasonList = [2018, 2019]
-positions = ['RB', 'QB', 'WR', 'TE']
+positions = ["RB", "QB", "WR", "TE"]
 points = {
-    'Pass Yds':     0.04,
-    'Pass TD':      4,
-    'Int':          -1,
-    'Rush Att':     0.1,
-    'Rush TD':      6,
-    'Ret TD':       6,
-    'Rec':          1,
-    'Rec Yds':      0.1,
-    'Rec TD':       6,
-    '2-PT':         2,
-    'Fum Lost':     -2,
-    'Fum Ret TD':   2
+    "Pass Yds": 0.04,
+    "Pass TD": 4,
+    "Int": -1,
+    "Rush Att": 0.1,
+    "Rush TD": 6,
+    "Ret TD": 6,
+    "Rec": 1,
+    "Rec Yds": 0.1,
+    "Rec TD": 6,
+    "2-PT": 2,
+    "Fum Lost": -2,
+    "Fum Ret TD": 2,
 }
+
+# connect('private.json')
